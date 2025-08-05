a6c9c34feb42b8fa2d38e6017720e21daef07e03
Bob Green <bob.green@resillion.com>
2025-08-05 07:59:02 +0000

    Added 'unique' constraint to tape label.

--- Doo/models.py
+++ Doo/models.py
@@ -24,7 +24,7 @@ class StorageLocation(models.Model):
         return f"{self.name}"
 
 class Tape(models.Model):
-    label = models.CharField(max_length=30)
+    label = models.CharField(max_length=30, unique=True, null=True, default=None)
     media_type = models.ForeignKey(MediaType, on_delete=models.RESTRICT)
     location = models.ForeignKey(StorageLocation, on_delete=models.RESTRICT, default=-1)
     date_moved = models.DateTimeField(auto_now_add=True, null=True)
